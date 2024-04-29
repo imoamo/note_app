3a5c5f31499e05be067555e4888509a16f52da6c
imoeee <112760500+imoeee@users.noreply.github.com>
2024-04-29 06:10:51 +0530

    done

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const dbConnect = require('./config/db');
 const userRouter = require('./routes/userRouter');
 const noteRouter = require('./routes/noteRouter');
-const cors = require('cors');
-const app = express();
 require('dotenv').config();
+
+const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", " *");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+    next();
+});
+
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
